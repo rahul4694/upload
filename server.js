97e9739dea0b63706a088b69e4d41b66dcd3407a
rahul4694 <reticentrahul@gmail.com>
2020-01-24 19:22:44 +0530

    second

--- server.js
+++ server.js
@@ -15,15 +15,11 @@ app.post("/startUpload", (req, res, next) => {
   };
   try {
     const Stat = fs.statSync("FileStorage/" + req.body.name);
-    console.log(Stat);
     if (Stat.isFile()) {
       Files[req.body.name]["download"] = Stat.size;
       Files[req.body.name]["place"] = Stat.size / 1024000;
     }
-    console.log(Files[req.body.name]["place"]);
-  } catch (err) {
-      console.log(err.message)
-  }
+  } catch (err) {}
   fs.open("FileStorage/" + req.body.name, "a", 0755, (err, fdata) => {
     if (err) return console.log(err.message);
     Files[req.body.name]["Handler"] = fdata;
@@ -37,7 +33,39 @@ app.post("/startUpload", (req, res, next) => {
   });
 });
 
-app.post("/continueUpload", (req, res) => {});
+app.post("/continueUpload", (req, res) => {
+  console.log(req.body);
+  const Name = req.body.name;
+  Files[Name]["download"] += req.body.chunk.length;
+  Files[Name]["data"] += req.body.chunk;
+  if (Files[Name]["download"] == Files[Name]["filesize"]) {
+    fs.write(
+      Files[Name]["Handler"],
+      Files[Name]["data"],
+      null,
+      "Binary",
+      function(err, Writen) {
+        res.send(["Done"]);
+      }
+    );
+  } else if (Files[Name]["data"].length >= 10240000) {
+    Files[Name]["data"] = "";
+    fs.write(
+      Files[Name]["Handler"],
+      Files[Name]["data"],
+      null,
+      "Binary",
+      function(err, Writen) {
+        res.send(["More_data"]);
+      }
+    );
+  } else {
+    var place = Files[Name]["download"] / 1024000;
+    console.log(place);
+    // var Percent = (Files[Name]["Downloaded"] / Files[Name]["FileSize"]) * 100;
+    res.send(["More_data", { place: place }]);
+  }
+});
 
 app.listen(5200, () => {
   console.log("server is running at 5200");
